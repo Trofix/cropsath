f9ee6c070cbd96d58c592cc24b9995b1b585b455
Móricz Gergő <moriczgergo05@gmail.com>
2017-01-21 22:35:47 +0100

    Created default includes file

---
+++ include.php
@@ -0,0 +1,28 @@
+<?php
+//THIS FILE IS A PART OF CROPSATH, LICENSED UNDER GPLv3.
+//MADE BY MORICZGERGO A.K.A. SKIILAA
+//CREATED: 2017.01.21.
+//LAST MODIFIED: 2017.01.21.
+
+$config = json_decode(file_get_contents("config.json"));
+?>
+<html>
+    <head>
+       <meta charset="UTF-8">
+       <title><?php echo $config->pageName ?></title>
+       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+    </head>
+    <body>
+        <div class="header">
+           <center>
+               <img src="logo.png">
+               <hr>
+           </center>
+        </div>
+        
+        <!-- BOOTSTRAP 4 !-->
+        <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+    </body>
+</html>
