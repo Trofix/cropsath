8c60b2690648c0ef3a1920dda4e3b5f1fff5d22d
Móricz Gergő <moriczgergo05@gmail.com>
2017-01-22 09:03:50 +0100

    Created index.php!

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+//THIS FILE IS A PART OF CROPSATH, LICENSED UNDER GPLv3.
+//MADE BY MORICZGERGO A.K.A. SKIILAA
+//CREATED: 2017.01.22.
+//LAST MODIFIED: 2017.01.22.
+
+include_once "include.php";
+
+$conn = new mysqli($config->sqlserv, $config->sqluser, $config->sqlpass, $config->pageName);
+
+if ($conn->connecterror){
+  echo "<center><h1>Failed to connect to database.</h1></center>";
+  die();
+}
+
+$sql = "SELECT questionName FROM questions ORDER BY id DESC";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0){
+  //output data of each row in bootstrap cards
+  while($row = $result->fetch_assoc()) {
+    echo "<div class=\"card\"><div class=\"card-block\"><h4 class=\"card-text\">" . $row["questionText"] . "</h4></div></div>";
+  }
+} else {
+  echo "<center><h1>No questions found.</h1></center>";
+}
+?>
