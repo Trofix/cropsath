40ee39fbdd13dea57cf58b85c040fdc472d97989
Móricz Gergő <moriczgergo05@gmail.com>
2017-01-22 10:09:18 +0100

    Some more tricky stuff

--- index.php
+++ index.php
@@ -18,10 +18,12 @@ $sql = "SELECT questionName FROM questions ORDER BY id DESC";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0){
+  echo "<div style=\"width:100%\">"
   //output data of each row in bootstrap cards
   while($row = $result->fetch_assoc()) {
     echo "<div class=\"card\" style=\"width: 20rem; display: table-cell;\"><div class=\"card-block\"><h4 class=\"card-text\">" . $row["questionName"] . "</h4></div></div>";
   }
+  echo "</div>";
 } else {
   echo "<center><h1>No questions found.</h1></center>";
 }
