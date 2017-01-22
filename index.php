f36d2bab4b314e85a601dab436d82ed5a94978b5
Móricz Gergő <moriczgergo05@gmail.com>
2017-01-22 10:23:39 +0100

    Update index.php

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ if ($result->num_rows > 0){
   echo "<div class=\"responsive-thingy\">";
   //output data of each row in bootstrap cards
   while($row = $result->fetch_assoc()) {
-    echo "<div class=\"card\" style=\"width: 20rem; display: table-cell;\"><div class=\"card-block\"><h4 class=\"card-text\">" . $row["questionName"] . "</h4></div></div><br class=\"rwd-break\">";
+    echo "<div class=\"card\" style=\"width: 20rem; display: table-cell;\"><div class=\"card-block\"><h4 class=\"card-text\">" . $row["questionName"] . "</h4></div></div>";
   }
   echo "</div>";
 } else {
