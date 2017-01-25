c012761a272b6c8ba2d5b7b4ef96cd6f769ea9e2
Móricz Gergő <moriczgergo05@gmail.com>
2017-01-25 20:01:33 +0100

    Added return links to ask backplane

--- ask_backplane.php
+++ ask_backplane.php
@@ -4,19 +4,19 @@
 include_once "include.php";
 session_start();
 if (!isset($_POST["qname"]) || !isset($_POST["qtext"]) || strlen(trim($_POST["qname"])) == 0 || strlen(trim($_POST["qtext"])) == 0) {
-  echo "<center><h1>Required fields are not specified.</h1></center>";
+  echo "<center><h1>Required fields are not specified.</h1><br><h2><a href=\"ask.php\" class=\"btn btn-primary\">Back to ask form.</a></h2></center>";
   die();
 }
 session_start();
 
 if ($_SESSION["admin"] == 0){
-  echo "<center><h1>You are not allowed to do this.</h1></center>";
+  echo "<center><h1>You are not allowed to do this.</h1><br><h2><a href=\"index.php\" class=\"btn btn-primary\">Back to homepage.</a></h2></center>";
   die();
 }
 
 $conn = new mysqli($config->sqlServ, $config->sqlUser, $config->sqlPass, $config->dbName);
 if ($conn->connect_error){
-  echo "<center><h1>Failed to connect to database.</h1></center>";
+  echo "<center><h1>Failed to connect to database.</h1><br><h2><a href=\"ask.php\" class=\"btn btn-primary\">Back to ask form.</a></h2></center>";
   echo $conn->connect_error;
   die();
 }
@@ -26,7 +26,7 @@ $stmt = $conn->prepare($sql);
 $stmt->bind_param("i", $_SESSION["uid"]);
 $result = $stmt->execute();
 if ($result === FALSE) {
-  echo "<center><h1>An error happened with the SQL query.</h1><h2>" . $conn->error . "</h2></center>";
+  echo "<center><h1>An error happened with the SQL query.</h1><h2>" . $conn->error . "</h2><br><h2><a href=\"ask.php\" class=\"btn btn-primary\">Back to ask form.</a></h2></center>";
   die();
 }
 
@@ -36,12 +36,12 @@ $stmt->store_result();
 if ($stmt->num_rows > 0) {
   $stmt->fetch();
 } else {
-  echo "<center><h1>You were not found.</h1></center>";
+  echo "<center><h1>You were not found.</h1><br><h2><a href=\"login.php\" class=\"btn btn-primary\">Please log in again.</a></h2></center>";
   die();
 }
 
 if ($admin == 0){
-  echo "<center><h1>You are not allowed to do this.</h1></center>";
+  echo "<center><h1>You are not allowed to do this.</h1><br><h2><a href=\"index.php\" class=\"btn btn-primary\">Back to homepage.</a></h2></center>";
   die();
 }
 
@@ -50,9 +50,9 @@ $stmt = $conn->prepare($sql);
 $stmt->bind_param("ss", $_POST["qname"], $_POST["qtext"]);
 $result = $stmt->execute();
 if ($result === FALSE) {
-  echo "<center><h1>An error happened with the SQL query.</h1><h2>" . $conn->error . "</h2></center>";
+  echo "<center><h1>An error happened with the SQL query.</h1><h2>" . $conn->error . "</h2><br><h2><a href=\"ask.php\" class=\"btn btn-primary\">Back to ask form.</a></h2></center>";
   die();
 } else {
-  echo "<center><h1>Asking successful!</h1></center>";
+  echo "<center><h1>Asking successful!</h1><br><h2><a href=\"index.php\" class=\"btn btn-primary\">Back to homepage.</a></h2></center>";
 }
 ?>
