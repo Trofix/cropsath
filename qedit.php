055024b9c1ba1c2cf9732e4405cdbee1171f5671
Móricz Gergő <moriczgergo05@gmail.com>
2017-01-25 20:56:12 +0100

    Added qedit frontend page

---
+++ qedit.php
@@ -0,0 +1,20 @@
+<?php
+//THIS FILE IS A PART OF CROPSATH, LICENSED UNDER GPLv3.
+//MADE BY MORICZGERGO A.K.A. SKIILAA
+include_once "include.php";
+?>
+
+<html>
+  <body>
+    <center>
+      <form action="qedit_backplane.php" method="POST">
+        <p>Question name:</p>
+        <input type="text" name="qname">
+        <p>Question text:</p>
+        <textarea name="qtext" cols="100" rows="5"></textarea>
+        <br><br>
+        <input type="submit" name="submit" value="Edit" class="btn btn-primary">
+      </form>
+    </center>
+  </body>
+</html>
