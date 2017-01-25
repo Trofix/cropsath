d6b70073528d910678d37857d360f51315d448a6
Móricz Gergő <moriczgergo05@gmail.com>
2017-01-25 21:19:39 +0100

    Added langfile-specific text in register form

--- register.php
+++ register.php
@@ -8,14 +8,14 @@ include_once "include.php";
   <body>
     <center>
       <form action="register_backplane.php" method="POST">
-        <p>Username:</p>
+        <p><?php echo $langfile->username; ?></p>
         <input type="text" name="username" required>
-        <p>Password:</p>
+        <p><?php echo $langfile->password; ?></p>
         <input type="password" name="password" required>
-        <p>E-mail address:</p>
+        <p><?php echo $langfile->email_address; ?></p>
         <input type="email" name="email" required>
         <br><br>
-        <input type="submit" name="submit" value="Register" class="btn btn-primary">
+        <input type="submit" name="submit" value="<?php echo $langfile->register; ?>" class="btn btn-primary">
       </form>
     </center>
   </body>
