c174e0b04125ed0aee7433c995f2a73b524ce354
Móricz Gergő <moriczgergo05@gmail.com>
2017-01-25 18:00:13 +0100

    Created login backplane

---
+++ login_backplane.php
@@ -0,0 +1,47 @@
+<?php
+//THIS FILE IS A PART OF CROPSATH, LICENSED UNDER GPLv3.
+//MADE BY MORICZGERGO A.K.A. SKIILAA
+
+include_once "include.php";
+
+session_start();
+
+if (!isset($_GET["id"])) {
+  echo "<center><h1>Question not specified.</h1></center>";
+  die();
+}
+session_start();
+$conn = new mysqli($config->sqlServ, $config->sqlUser, $config->sqlPass, $config->dbName);
+if ($conn->connect_error){
+  echo "<center><h1>Failed to connect to database.</h1></center>";
+  echo $conn->connect_error;
+  die();
+}
+$sql = "SELECT id, password FROM users WHERE username = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("s", $_POST["username"]);
+$result = $stmt->execute();
+if ($result === FALSE) {
+  echo "<center><h1>An error happened with the SQL query.</h1><h2>" . $conn->error . "</h2></center>";
+  die();
+}
+$stmt->bind_result($id, $password);
+$stmt->store_result();
+if ($stmt->num_rows == 1){
+  $stmt->fetch();
+  if (!password_verify($_POST["password"] , $password)){
+    echo "<center><h1>Wrong password.</h1></center>";
+    die();
+  }
+  $_SESSION["uid"] = $id;
+  $_SESSION["uname"] = $_POST["username"];
+  echo "<center><h1>Login successful!</h1></center>";
+} else {
+  if ($stmt->num_rows == 0){
+    echo "<center><h1>User not found.</h1></center>";
+  } else {
+    echo "<center><h1>An unexpected error happened.</h1><h2>Code: multiple_users</h2></center>";
+  }
+  die();
+}
+?>
