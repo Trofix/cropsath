575a60061f55ed09195b72f131256ad6e989557b
Móricz Gergő <moriczgergo05@gmail.com>
2017-01-25 17:53:50 +0100

    Added frontend registration page

---
+++ register.php
@@ -0,0 +1,22 @@
+<?php
+//THIS FILE IS A PART OF CROPSATH, LICENSED UNDER GPLv3.
+//MADE BY MORICZGERGO A.K.A. SKIILAA
+include_once "include.php";
+?>
+
+<html>
+  <body>
+    <center>
+      <form action="login_backplane.php" method="POST">
+        <p>Username:</p>
+        <input type="text" name="username" required>
+        <p>Password:</p>
+        <input type="password" name="password" required>
+        <p>E-mail address:</p>
+        <input type="text" name="email" required>
+        <br>
+        <input type="submit" value="Register">
+      </form>
+    </center>
+  </body>
+</html>
