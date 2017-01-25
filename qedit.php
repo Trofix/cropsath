4d1cc508acc1549594b3d5e5200d51c9e76ddaea
Móricz Gergő <moriczgergo05@gmail.com>
2017-01-25 21:08:55 +0100

    Edit page now shows existing text

--- qedit.php
+++ qedit.php
@@ -2,20 +2,53 @@
 //THIS FILE IS A PART OF CROPSATH, LICENSED UNDER GPLv3.
 //MADE BY MORICZGERGO A.K.A. SKIILAA
 include_once "include.php";
-?>
 
+if (!isset($_GET["id"])) {
+  echo "<center><h1>Question not specified.</h1></center>";
+  die();
+}
+session_start();
+$conn = new mysqli($config->sqlServ, $config->sqlUser, $config->sqlPass, $config->dbName);
+if ($conn->connect_error){
+  echo "<center><h1>Failed to connect to database.</h1></center>";
+  echo $conn->connect_error;
+  die();
+}
+$sql = "SELECT questionName, questionText FROM questions WHERE id = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("s", $_GET["id"]);
+$result = $stmt->execute();
+if ($result === FALSE) {
+  echo "<center><h1>An error happened with the SQL query.</h1><h2>" . $conn->error . "</h2></center>";
+  die();
+}
+$stmt->bind_result($questionName, $questionText);
+$stmt->store_result();
+if ($stmt->num_rows == 1){
+  $stmt->fetch();
+?>
 <html>
   <body>
     <center>
       <form action="qedit_backplane.php" method="POST">
         <input type="hidden" name="qid" value="<?php echo $_GET["id"]; ?>">
         <p>Question name:</p>
-        <input type="text" name="qname">
+        <input type="text" name="qname" value="<?php echo $questionName; ?>">
         <p>Question text:</p>
-        <textarea name="qtext" cols="100" rows="5"></textarea>
+        <textarea name="qtext" cols="100" rows="5"><?php echo $questionText; ?></textarea>
         <br><br>
         <input type="submit" name="submit" value="Edit" class="btn btn-primary">
       </form>
     </center>
   </body>
 </html>
+<?php
+} else {
+  if ($stmt->num_rows == 0){
+    echo "<center><h1>Question not found.</h1></center>";
+  } else {
+    echo "<center><h1>An unexpected error happened.</h1><h2>Code: multiple_questions</h2></center>";
+  }
+  die();
+}
+?>
