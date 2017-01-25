0db38f394d4446fa6d1ada95d0a6a8999a68122c
Móricz Gergő <moriczgergo05@gmail.com>
2017-01-25 17:10:12 +0100

    Create addcomment.php

---
+++ addcomment.php
@@ -0,0 +1,85 @@
+<?php
+//THIS FILE IS A PART OF CROPSATH, LICENSED UNDER GPLv3.
+//MADE BY MORICZGERGO A.K.A. SKIILAA
+
+include_once "include.php";
+
+session_start();
+
+//Check if user is logged in
+if (!isset($_SESSION["uname"]) && !isset($_SESSION["uid"]) && strlen(trim($_SESSION["uname"])) && strlen(trim($_SESSION["uid"]))) {
+  echo "<center><h1>You are not logged in.</h1></center>";
+  die();
+}
+
+//Check if needed vars are specified
+if (!isset($_POST["id"]) && !isset($_POST["comment"]) && strlen(trim($_POST["id"])) && strlen(trim($_POST["comment"]))) {
+  echo "<center><h1>You haven't specified all needed fields.</h1></center>";
+  die();
+}
+
+//Init MYSQLi
+$conn = new mysqli($config->sqlServ, $config->sqlUser, $config->sqlPass, $config->dbName);
+if ($conn->connect_error){
+  echo "<center><h1>Failed to connect to database.</h1></center>";
+  echo $conn->connect_error;
+  die();
+}
+
+//Select comments
+$sql = "SELECT comments FROM questions WHERE id = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("s", $_POST["id"]);
+$result = $stmt->execute();
+
+if ($result === FALSE) {
+  echo "<center><h1>An error happened with the SQL query.</h1><h2>" . $conn->error . "</h2></center>";
+  die();
+}
+
+$stmt->bind_result($comments);
+$stmt->store_result();
+
+if ($stmt->num_rows == 1){
+  //Fetch results
+  $stmt->fetch();
+  
+  //Decode JSON
+  $commentObject = json_decode($comments);
+  
+  //If JSON decode failed, create false commentObject
+  if($commentObject === NULL){
+    $commentObject = (object)array("comments" => array());
+  }
+  
+  $commentsArray = $commentObject->comments;
+  
+  $newArray = array("user" => $_SESSION["uname"], "id" => $_SESSION["uid"], "text" => $_POST["comment"]);
+  
+  array_push($commentsArray, $newArray);
+  
+  $commentObject->comments = $commentsArray;
+  
+  $newComments = json_encode($commentObject);
+  
+  //New SQL for updating comments
+  $sql = "UPDATE questions SET comments = ? WHERE id = ?";
+  $stmt = $conn->prepare($sql);
+  $stmt->bind_param("ss", $newComments, $_POST["id"]);
+  $result = $stmt->execute();
+  
+  //Error checking
+  if ($result === FALSE) {
+    echo "<center><h1>An error happened with the second SQL query.</h1><h2>" . $conn->error . "</h2></center>";
+    die();
+  } else {
+    echo "<center><h1>Commenting successful!</h1></center>";
+  }
+} else {
+  if ($stmt->num_rows == 0){
+    echo "<center><h1>Question not found.</h1></center>";
+  } else {
+    echo "<center><h1>An unexpected error happened.</h1><h2>Code: multiple_questions</h2></center>";
+  }
+  die();
+}
