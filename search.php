78ae90c3926afcde73c34e9d68529c927029790e
Móricz Gergő <moriczgergo05@gmail.com>
2017-01-26 15:08:01 +0100

    Search form

---
+++ search.php
@@ -0,0 +1,21 @@
+<?php
+//THIS FILE IS A PART OF CROPSATH, LICENSED UNDER GPLv3.
+//MADE BY MORICZGERGO A.K.A. SKIILAA
+//CREATED: 2017.01.22.
+//LAST MODIFIED: 2017.01.22.
+
+include_once "include.php";
+?>
+
+<html>
+  <body>
+    <center>
+      <form action="search_backplane.php" method="GET">
+        <p><?php echo $langfile->query; ?></p>
+        <input type="text" name="q">
+        <br><br>
+        <input type="submit" name="submit" value="<?php echo $langroom->search; ?>" class="btn btn-primary">
+      </form>
+    </center>
+  </body>
+</html>
