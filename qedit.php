5d658342f0c7bd3c42637982757d697d141aa99d
Móricz Gergő <moriczgergo05@gmail.com>
2017-01-26 10:52:47 +0100

    Langsupp

--- qedit.php
+++ qedit.php
@@ -4,13 +4,13 @@
 include_once "include.php";
 
 if (!isset($_GET["id"])) {
-  echo "<center><h1>Question not specified.</h1></center>";
+  echo "<center><h1>" . $langfile->q_not_spec . "</h1></center>";
   die();
 }
 session_start();
 $conn = new mysqli($config->sqlServ, $config->sqlUser, $config->sqlPass, $config->dbName);
 if ($conn->connect_error){
-  echo "<center><h1>Failed to connect to database.</h1></center>";
+  echo "<center><h1>" . $langfile->db_conn_fail . "</h1></center>";
   echo $conn->connect_error;
   die();
 }
@@ -19,7 +19,7 @@ $stmt = $conn->prepare($sql);
 $stmt->bind_param("s", $_GET["id"]);
 $result = $stmt->execute();
 if ($result === FALSE) {
-  echo "<center><h1>An error happened with the SQL query.</h1><h2>" . $conn->error . "</h2></center>";
+  echo "<center><h1>" . $langfile->sql_query_error . "</h1><h2>" . $conn->error . "</h2></center>";
   die();
 }
 $stmt->bind_result($questionName, $questionText);
@@ -32,12 +32,12 @@ if ($stmt->num_rows == 1){
     <center>
       <form action="qedit_backplane.php" method="POST">
         <input type="hidden" name="qid" value="<?php echo $_GET["id"]; ?>">
-        <p>Question name:</p>
+        <p><?php echo $langfile->q_name; ?></p>
         <input type="text" name="qname" value="<?php echo $questionName; ?>">
-        <p>Question text:</p>
+        <p><?php echo $langfile->q_name; ?></p>
         <textarea name="qtext" cols="100" rows="5"><?php echo $questionText; ?></textarea>
         <br><br>
-        <input type="submit" name="submit" value="Edit" class="btn btn-primary">
+        <input type="submit" name="submit" value="<?php echo $langfile->edit; ?>" class="btn btn-primary">
       </form>
     </center>
   </body>
@@ -45,9 +45,9 @@ if ($stmt->num_rows == 1){
 <?php
 } else {
   if ($stmt->num_rows == 0){
-    echo "<center><h1>Question not found.</h1></center>";
+    echo "<center><h1>" . $langfile->q_not_found . "</h1></center>";
   } else {
-    echo "<center><h1>An unexpected error happened.</h1><h2>Code: multiple_questions</h2></center>";
+    echo "<center><h1>" . $langfile->unx_err . "</h1><h2>" . $langfile->code . " multiple_questions</h2></center>";
   }
   die();
 }
